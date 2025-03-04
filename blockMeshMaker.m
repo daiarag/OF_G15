df91bfbeee9431f6ffe6886ed0596a80fd826f2c
ngly712 <ntl562@my.utexas.edu>
2025-03-04 10:37:34 -0600

    Streamlined vertex making process, should be easier to make blocks from vertices matrix now -- bug-tested for different angular partitions

--- blockMeshMaker.m
+++ blockMeshMaker.m
@@ -12,10 +12,14 @@
 % is [4 1])
 % The function will save a file to MATLAB directory (where this function
 % rests) and display the following:
+% Number of vertices
 % Number of radial blocks
 % Number of rectangular blocks
-% Number of inlet/outlet faces
+% Number of inlet faces
+% Number of outlet faces
 % Number of cylinder faces
+% Number of top faces
+% Number of bottom faces
 function [] = blockMeshMaker(param)
     arguments
         param.nAngles double {mustBeInteger, mustBeFinite, mustBeScalarOrEmpty,...
@@ -34,116 +38,98 @@ function [] = blockMeshMaker(param)
             mustBePositive} = 5;
     end
     fid = fopen("blockMeshDict", "w+"); % Create new file to write to
+    % Vertex determination - cylinder first
+    theta = 360 / param.nAngles; % angle increment
+    vertices = zeros(16 + 6 * param.nAngles, 3); % Matrix to hold vertices
+    zCounter = length(vertices) / 2;
+    k = 1; % For indexing
+    % Circular vertices
+    for i = 0:param.nAngles - 1
+        % Cylinder
+        vertices(i + 1, :) = [0.5 * cosd(theta * i) ...
+            0.5 * sind(theta * i) -0.5];
+        % Outer ring
+        vertices(i + param.nAngles + 1, :) = [(0.5 + ...
+            param.R) * cosd(theta * i) (0.5 + param.R) * sind(theta...
+            * i) -0.5];
+    end
+    k = k + 2 * (i + 1);
+    % Top-back domain vertices
+    i = 0;
+    % Checking when to terminate
+    while (cosd(i * theta) > 0)
+        vertices(i + k, :) = [param.Lw (0.5 + param.R)...
+            * sind(theta * i) -0.5];
+        i = i + 1;
+    end
+    % Top-right corner
+    vertices(i + k, :) = [param.Lw param.H -0.5];
+    k = k + i + 1;
+    % Top domain vertices
+    if (mod(param.nAngles, 4) == 0)
+        for j = 1:-1:-1
+            vertices(k + 1 - j, :) = [(0.5 + param.R)...
+                * cosd(90 - theta * j) param.H -0.5];
+        end
+        k = k + 3;
+        i = i + 1;
+    else
+        vertices(k, :) = [(0.5 + param.R) * cosd(theta...
+            * (i - 1)) param.H -0.5];
+        vertices(k + 1, :) = [(0.5 + param.R) * ...
+            cosd(theta * i) param.H -0.5];
+        k = k + 2;
+    end
+    % Front domain vertices
+    % Top-left corner
+    vertices(k, :) = [-param.Lf param.H -0.5];
+    k = k + 1;
+    j = 0;
+    while (cosd(i * theta) < 0)
+        vertices(j + k, :) = [-param.Lf (0.5 + param.R)...
+            * sind(theta * i) -0.5];
+        j = j + 1;
+        i = i + 1;
+    end
+    % Bottom-left corner
+    vertices(j + k, :) = [-param.Lf -param.H -0.5];
+    k = k + j + 1;
+    % Bottom domain vertices
+    if (mod(param.nAngles, 4) == 0)
+        for j = 1:-1:-1
+            vertices(k + 1 - j, :) = [(0.5 + param.R)...
+                * cosd(270 - theta * j) -param.H -0.5];
+        end
+        k = k + 3;
+        i = i + 1;
+    else
+        vertices(k, :) = [(0.5 + param.R) * cosd(theta...
+            * (i - 1)) -param.H -0.5];
+        vertices(k + 1, :) = [(0.5 + param.R) * ...
+            cosd(theta * i) -param.H -0.5];
+        k = k + 2;
+    end
+    % Bottom-right corner
+    vertices(k, :) = [param.Lw -param.H -0.5];
+    k = k + 1;
+    j = 0;
+    % Bottom-back domain vertices
+    while (theta * i < 360)
+        vertices(k + j, :) = [param.Lw (0.5 + param.R)...
+            * sind(theta * i) -0.5];
+        i = i + 1;
+        j = j + 1;
+    end
+    vertices(zCounter + 1:end, :) = [vertices(1:zCounter, 1:2) ...
+        0.5 * ones(zCounter, 1)];
+    plot(vertices(1:zCounter, 1), vertices(1:zCounter, 2), 'o');
     % Constant lines of every blockMeshDict
     fprintf(fid, "FoamFile\n{\n\tversion:\t2.0;\n\tformat:\tascii;\n"...
         +"\tclass:\tdictionary;\n\tobject:\tblockMeshDict;\n}\n\n");
     fprintf(fid, "convertToMeters:\t1.0;\n\n");
+    % Print vertices from list
     fprintf(fid, "vertices\n(\n");
-    % Vertex determination - cylinder first
-    theta = 360 / param.nAngles; % angle increment
-    total = 0; % For labeling vertices
-    k = 0; % For total counter
-    % First front, then back
-    % Uncomment these and any plot3 to visualize
-    % figure;
-    % hold on;
-    for n = 0:1
-        % Wall vertices
-        for i = 0:param.nAngles - 1
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", 0.5 * ...
-                cosd(theta * i), 0.5 * sind(theta * i), n, i + total);
-            % plot3(0.5*cosd(theta*i),0.5*sind(theta*i),n,'o');
-        end
-        k = i + 1; % For total counter
-        % Outer radius vertices
-        for i = 0:param.nAngles - 1
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", (0.5 + ...
-                param.R) * cosd(theta * i), (0.5 + param.R) * sind(theta...
-                * i), n, i + k + total);
-            % plot3((0.5+param.R)*cosd(theta*i),(0.5+param.R)*sind(theta*i),n,'o');
-        end
-        k = k + i + 1; % For total counter
-        % Top-back domain vertices
-        i = 0; % Checking when to terminate
-        while (cosd(i * theta) > 0)
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", param.Lw, ...
-                (0.5 + param.R) * sind(theta * i), n, i + k + total);
-            % plot3(param.Lw,(0.5+param.R)*sind(theta*i),n,'o');
-            i = i + 1;
-        end
-        fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", param.Lw, ...
-            param.H, n, i + k + total); % Top-right corner
-        % plot3(param.Lw,param.H,n,'o');
-        k = k + i + 1; % For total counter
-        % Top domain vertices
-        if (mod(param.nAngles, 4) == 0)
-            for j = 1:-1:-1
-                fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", (0.5 +...
-                    param.R) * cosd(90 - theta * j), param.H, n, k + 1 -...
-                    j + total);
-                % plot3((0.5+param.R)*cosd(90-theta*j),param.H,n,'o');
-            end
-            k = k + 3;
-            i = i + 1;
-        else
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", (0.5 + ...
-                param.R) * cosd(theta * i - 1), param.H, n, k + total);
-            % plot3((0.5+param.R)*cosd(theta*i-1),param.H,n,'o');
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", (0.5 + ...
-                param.R) * cosd(theta * i), param.H, n, k + total + 1);
-            % plot3((0.5+param.R)*cosd(theta*i),param.H,n,'o');
-            k = k + 2;
-        end
-        % Front domain vertices
-        fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", -param.Lf, ...
-            param.H, n, k + total); % Top-left corner
-        % plot3(-param.Lf,param.H,n,'o');
-        k = k + 1; % For total counter
-        j = 0; % For labeling front wall
-        while (cosd(i * theta) < 0)
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", -param.Lf, ...
-            (0.5 + param.R) * sind(theta * i), n, j + k + total);
-            % plot3(-param.Lf,(0.5+param.R)*sind(theta*i),n,'o');
-            j = j + 1;
-            i = i + 1;
-        end
-        fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n",-param.Lf, ...
-            -param.H, n, k + j + total); % Bottom-left corner
-        % plot3(param.Lf,-param.H,n,'o');
-        k = k + j + 1; % For total counter
-        % Bottom domain vertices
-        if (mod(param.nAngles, 4) == 0)
-            for j = 1:-1:-1
-                fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", (0.5 +...
-                    param.R) * cosd(270 - theta * j), -param.H, n, k + 1 -...
-                    j + total);
-                % plot3((0.5+param.R)*cosd(270-theta*j),-param.H,n,'o');
-            end
-            k = k + 3;
-            i = i + 1;
-        else
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", (0.5 + ...
-                param.R) * cosd(theta * i - 1), -param.H, n, k + total);
-            % plot3((0.5+param.R)*cosd(theta*i-1),-param.H,n,'o');
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", (0.5 + ...
-                param.R) * cosd(theta * i), -param.H, n, k + total + 1);
-            % plot3((0.5+param.R)*cosd(theta*i),-param.H,n,'o');
-            k = k + 2;
-        end
-        fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", param.Lw, ...
-            -param.H, n, k + total); % Bottom-right corner
-        % plot3(param.Lw,-param.H,n,'o');
-        k = k + 1; % For total counter
-        j = 0; % For labeling back wall
-        % Bottom-back domain vertices
-        while (theta * i < 360)
-            fprintf(fid, "\t(%1.10f %2.10f %3.1f) \\\\%4.0f\n", param.Lw, ...
-                (0.5 + param.R) * sind(theta * i), n, j + k + total);
-            % plot3(param.Lw,(0.5+param.R)*sind(theta*i),n,'o');
-            i = i + 1;
-            j = j + 1;
-        end
-        total = k + j;
-    end
+    fprintf(fid, "\t(%1.10f %2.10f %3.10f) \\\\%4.0f\n", [vertices'; ...
+        0:15 + 6 * param.nAngles]);
     fprintf(fid, ");\n\n");
     fclose(fid); % Close file
